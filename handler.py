5692a9379f68688a36b1509ebe33bf043f064ee2
Sergey A <varnie29a@mail.ru>
2020-09-22 12:23:44 +0500

    add better errors handling

--- handler.py
+++ handler.py
@@ -1,27 +1,91 @@
-from contextlib import redirect_stdout
 import json
+import os
+from contextlib import redirect_stdout
+from http import HTTPStatus
 from io import StringIO
+from pathlib import Path
 from typing import List
 
-from victoria.script import victoria
 from victoria import config
+from victoria.script import victoria
+
+PROJECT_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
+CONFIG_PATH = PROJECT_PATH / "victoria.yaml"
 
 
 def run_victoria(args: List[str]):
-    cfg = config.load("victoria.yaml")
+    """
+    Loads local config file and invokes Victoria module through
+    CLI capabilities with supplied arguments `args` and config.
+    Args:
+        args: List[str] arguments passed to Victoria.
+    Returns:
+        See :func:`handler` function below
+    """
+    cfg = config.load(CONFIG_PATH)
     victoria.cli.main(prog_name="victoria", args=args, obj=cfg)
 
 
+def read_output_val(f: StringIO) -> str:
+    """
+    Retrieves the content of StringIO object without trailing whitespace.
+    Args:
+        f: StringIO object
+    Returns:
+        String content of `f` StringIO object with trailing whitespace removed.
+    """
+    return f.getvalue().rstrip()
+
+
 def handler(event, context):
-    body = json.loads(event['body'])
+    """
+    Lambda function handler.
+    Executes Victoria and returns an appropriate HTTP response dict.
+    Reads Victoria's standard output stream for gathering text output of execution.
+
+    It assumes that Victoria performs sys.exit(...) upon execution, which results in SystemExit exception
+    with the code 0 designating success of operation and others standing for errors.
+    It returns BAD_REQUEST when this assumption is not met.
+
+    It returns INTERNAL_SERVER_ERROR status code when Victoria responded with the code other than 0
+    or when an exception other than SystemExit has been thrown during execution.
+
+    It provides a "message" field in HTTP response for failures of execution, which is built from
+    standard output stream or an exception details, if any.
+
+    Args:
+        event: Lambda-handler event data.
+        context: Lambda-handler runtime information (context object).
+    Returns:
+        A dict with status code and body or error details.
+    """
+    try:
+        body = json.loads(event['body'])
+    except (KeyError, ValueError) as e:
+        return {'statusCode': HTTPStatus.BAD_REQUEST,
+                'error': 'Invalid request',
+                'message': str(e)}
 
     f = StringIO()
     try:
         with redirect_stdout(f):
             run_victoria(body['args'])
-    except SystemExit:
-        pass
+    except SystemExit as e:
+        output = read_output_val(f)
 
-    response = {"statusCode": 200, "body": json.dumps(f.getvalue())}
+        if e.code == 0:
+            return {'statusCode': HTTPStatus.OK,
+                    'body': json.dumps(output)}
+        else:
+            return {'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR,
+                    'error': 'Command failed with non-zero status code',
+                    'message': output}
+    except Exception as e:
+        return {'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR,
+                'error': 'Command failed',
+                'message': str(e)}
 
-    return response
+    # For completeness: in case Victoria didn't exit with sys.exit(...) and didn't throw any exceptions
+    return {'statusCode': HTTPStatus.BAD_REQUEST,
+            'error': 'Invalid request',
+            'message': ''}
